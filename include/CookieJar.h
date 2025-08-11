347b536a4f1be3806051bd3d0de1539fab523b1a
Lilly Kunne <sikunne@c1r2s1.42wolfsburg.de>
2025-08-11 18:37:17 +0200

    [WIP] [#9] Initial setup of CookieJar class

--- include/CookieJar.h
+++ include/CookieJar.h
@@ -1,20 +1,44 @@
 /* --- CookieJar.h --- */
 
 /* ------------------------------------------
-Author: undefined
-Date: 8/10/2025
+author: Lilly81324
+date: 11/08/2025
 ------------------------------------------ */
 
 #ifndef COOKIEJAR_H
 #define COOKIEJAR_H
 
-class CookieJar {
-public:
-    CookieJar();
-    ~CookieJar();
+#include <map>
+#include <iostream>
+#include "Headers.h"
+// #include "CiLess.h"
 
-private:
+using namespace std;
 
+class CiLess;
+
+class CookieJar
+{
+	public:
+		CookieJar();
+		~CookieJar();
+
+		void	setCookieHeaders(Headers &h, string name, string value, string path, int maxAge, bool httpOnly, bool secure, string sameSite);
+		void	set(string name, string value);
+		void	erase(const string &key);
+		void	clear(void);
+
+		map<string, string>::const_iterator	CookieJar::getBegin(void) const;
+		map<string, string>::const_iterator	CookieJar::getEnd(void) const;
+		bool	keyExists(string key) const;
+		void	show(ostream &out) const;
+		int		getLength(void) const;
+		bool	isEmpty(void) const;
+		string	get(string key);
+	private:
+		map<string, string> cookies;
 };
+
+ostream	&operator<<(ostream, const CookieJar &target);
 
 #endif // COOKIEJAR_H
--- src/http/CookieJar.cpp
+++ src/http/CookieJar.cpp
@@ -1,16 +1,152 @@
 /* --- CookieJar.cpp --- */
 
 /* ------------------------------------------
-author: undefined
-date: 8/10/2025
+author: Lilly81324
+date: 11/08/2025
 ------------------------------------------ */
 
 #include "CookieJar.h"
+#include "Headers.h"
 
-CookieJar::CookieJar() {
-    // Constructor
+using namespace std;
+
+CookieJar::CookieJar()
+{
 }
 
-CookieJar::~CookieJar() {
-    // Destructor
+CookieJar::~CookieJar()
+{
+	this->cookies.clear();
+}
+
+/**
+ * ?
+ */
+void CookieJar::setCookieHeaders(\
+	Headers &h, string name, string value, string path, \
+	int maxAge, bool httpOnly, bool secure, string sameSite)
+{
+	(void)h;
+	(void)path;
+	(void)maxAge;
+	(void)httpOnly;
+	(void)secure;
+	(void)sameSite;
+	(this->cookies[name] = value);
+}
+
+/**
+ * Sets value of given key or adds a new pair
+ * @param key: Key by which to identify the field
+ * @param value: New value to set the keys value to
+ */
+void CookieJar::set(string key, string value)
+{
+	this->cookies[key] = value;
+}
+
+/**
+ * Removes key from CookieJar
+ * @param key: Key to remove
+ */
+void	CookieJar::erase(const string &key)
+{
+	this->cookies.erase(key);
+}
+
+/**
+ * Deletes all entries in CookieJar
+ */
+void	CookieJar::clear(void)
+{
+	this->cookies.clear();
+}
+
+/**
+ * @returns Constant Interator to beginning of CookieJar
+ */
+map<string, string>::const_iterator CookieJar::getBegin(void) const
+{
+	return (this->cookies.begin());
+}
+
+/**
+ * @returns Constant Interator to end of CookieJar
+ */
+map<string, string>::const_iterator CookieJar::getEnd(void) const
+{
+	return (this->cookies.end());
+}
+
+/**
+ * Returns the value for the given key and checks if it exists
+ * @param key: Key by which to identify the value from the cookies
+ * @param exists: Gets set to 1 if key exists, or 0 if not
+ * @returns Value at the given key or "" if non-existant
+ */
+bool CookieJar::keyExists(string key) const
+{
+	if (!this->cookies.count(key))
+		return (false);
+	return (true);
+}
+
+/**
+ * Displays the CookieJar on output
+ * @param out: Output stream to show on
+ */
+void	CookieJar::show(ostream &out) const
+{
+	map<string, string>::const_iterator it;
+
+	for(it = this->cookies.begin(); it != this->cookies.end(); it++)
+	{
+		if (it != this->cookies.begin())
+			out << endl;
+		out << "["  << it->first << "] : [" << it->second << "]";
+	}
+}
+
+/**
+ * @returns Amount of entries in CookieJar
+ */
+int	CookieJar::getLength(void) const
+{
+	int count = 0;
+	map<string, string>::const_iterator it;
+
+	for(it = this->getBegin(); it != this->getEnd(); it++)
+		count++;
+	return (count);
+}
+
+/**
+ * @returns Boolean wether cookies is empty or not
+ */
+bool	CookieJar::isEmpty(void) const
+{
+	return (this->cookies.empty());
+}
+
+/**
+ * Returns the value for the given key
+ * @param key: Key by which to identify the value from the cookies
+ * @returns Value at the given key or "" if non-existant
+ * @note For checking if key exists, use get(string, int)
+ * @note Cannot be a const function, due to how cookies indexing works
+ */
+string CookieJar::get(string key)
+{
+	if (!this->keyExists(key))
+		return ("");
+	return (this->cookies[key]);
+}
+
+/**
+ * Displays CookieJar on output
+ */
+ostream &operator<<(ostream &out, const CookieJar &target)
+{
+	target.show(out);
+	return (out);
 }

7f9f54f392ee918f184acc7d40f22300f5362b0f
onlyHydra <chiperstefantheodor@gmail.com>
2025-08-15 15:26:55 +0200

    Ok

--- tests/unit/test_virtual_server.cpp
+++ tests/unit/test_virtual_server.cpp
@@ -23,3 +23,24 @@ TEST_CASE("VirtualServer can store locations", "[VirtualServer]")
 	REQUIRE(vs.locations[0].path_prefix == "/api");
 	REQUIRE(vs.locations[0].root == "/var/www/api");
 }
+
+TEST_CASE("VirtualServer rejects invalid port", "[VirtualServer][invalid]") {
+    VirtualServer vs;
+    vs.listen_port = -1;
+    REQUIRE(vs.listen_port < 1);
+}
+
+TEST_CASE("VirtualServer allows empty server_names", "[VirtualServer]") {
+    VirtualServer vs;
+    REQUIRE(vs.server_names.empty());
+}
+
+TEST_CASE("VirtualServer handles many locations", "[VirtualServer]") {
+    VirtualServer vs;
+    for (int i = 0; i < 100; ++i) {
+        Location loc;
+        loc.path_prefix = "/loc" + std::string(1, 'A' + (i % 26));
+        vs.locations.push_back(loc);
+    }
+    REQUIRE(vs.locations.size() == 100);
+}

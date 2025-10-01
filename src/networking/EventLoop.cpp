726dd35af10f19729352b0130f5a83e91deb63db
Lilly Kunne <simkue2@web.de>
2025-10-01 14:08:31 +0200

    [#96] Made EventLoop prematurely quit drain period if no active Connections

--- src/networking/EventLoop.cpp
+++ src/networking/EventLoop.cpp
@@ -307,7 +307,7 @@ void EventLoop::run(int timeout_ms, Server *srv)
 void EventLoop::drain()
 {
 	unsigned long long end = TimeUtil::nowMs() + DRAIN_TIMEOUT_MS;
-	while (TimeUtil::nowMs() < end)
+	while (TimeUtil::nowMs() < end && _pfds.size() > 0)
 	{
 		// timer tick: let handlers enforce deadlines
 		for (size_t i = 0; i < _pfds.size(); ++i)
